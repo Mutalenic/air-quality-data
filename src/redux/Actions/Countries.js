4237ece16370df590d8a8a1200cf686c8672e2ca
Nicholas Mutale <nicomutale@gmail.com>
2022-04-28 18:58:25 +0200

    add actions for Countries and Pollution

---
+++ src/redux/Actions/Countries.js
@@ -0,0 +1,25 @@
+import { fetchCountries } from "../apiFunctions";
+
+export const ADD_COUNTRIES = 'air-quality-data/Countries/ADD_COUNTRIES';
+
+
+export const addCountries = (payload) => ({
+  type: ADD_COUNTRIES,
+  payload,
+});
+
+export const getCountries = (reg) => async (dispatch) => {
+  const countries = await fetchCountries(reg);
+  dispatch({
+    type: ADD_COUNTRIES,
+    payload: countries.map((country) => ({
+      name: country.name,
+      region: reg,
+      latlng: country.latlng,
+      population: country.population,
+      code: country.cca2,
+      flag: country.flags.png,
+    })),
+  });
+};
+
