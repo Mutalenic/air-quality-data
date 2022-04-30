5fbc1a3a631e1952d63cbd826992e2370f3e3af0
Nicholas Mutale <nicomutale@gmail.com>
2022-04-30 09:28:18 +0200

    fix linter error

--- src/tests/pollution.test.js
+++ src/tests/pollution.test.js
@@ -1,32 +1,32 @@
-import { render, cleanup } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import countriesReducer from "../redux/Reducers/Countries";
-import pollutionReducer from "../redux/Reducers/Pollution";
-import thunk from "redux-thunk";
+import { render, cleanup } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import { Provider } from "react-redux";
-import Pollutions from "../components/Home/Pollution";
+import { Provider } from 'react-redux';
+import pollutionReducer from '../redux/Reducers/Pollution';
+import countriesReducer from '../redux/Reducers/Countries';
+import Pollutions from '../components/Home/Pollution';
 
 const rootReducer = combineReducers({
-    countriesReducer,
-    pollutionReducer,
-  
-  });
-  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
-  
-  afterEach(cleanup);
+  countriesReducer,
+  pollutionReducer,
 
-  describe('Pollution list ', () => {
-      test('Pollution list renders correctly', () => {
-          const pollution = render(
-              <Provider store={store}>
-                  <Router>
-                      <Pollutions />
-                      </Router>
-                    </Provider>,
-          );
-          expect(pollution).toMatchSnapshot();
-      });
+});
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
+
+afterEach(cleanup);
+
+describe('Pollution list ', () => {
+  test('Pollution list renders correctly', () => {
+    const pollution = render(
+      <Provider store={store}>
+        <Router>
+          <Pollutions />
+        </Router>
+      </Provider>,
+    );
+    expect(pollution).toMatchSnapshot();
   });
+});
